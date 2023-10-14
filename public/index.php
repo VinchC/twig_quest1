77be79c5fcb1d60ce0c7ea32ce23a2c5a5390193
Vincent C <vinch.cm@gmail.com>
2023-10-14 15:29:48 +0200

    done

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@
 require_once __DIR__ . '/../config/twig.php';
 
 
-$name = 'Wilder';
+$name = 'Pops';
 
-echo $twig->render('home.html.twig', ['name' => $name]);
+$help = 'Help';
+
+echo $twig->render('home.html.twig', ['name' => $name, 'help' => $help]);
+
